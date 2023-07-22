6e40b0b253a9598eea309b686dc69c74de1a807a
Chadrac-WAKOMYA <chadracxx@gmail.com>
2023-07-22 19:11:32 +0200

    500 error amorced in createPokemon module

--- src/routes/createPokemon.js
+++ src/routes/createPokemon.js
@@ -7,5 +7,10 @@ module.exports = (app) => {
         const message = `Le pokémon ${req.body.name} a bien été crée.`
         res.json({ message, data: pokemon })
       })
+      .catch(error => {
+        const message = `Le pokemon n'a pas pu être ajouté. Réessayez un peu plus tard`
+        const statusCode = '500'
+        res.status(500).json({statusCode, message, data: error})
+      })
   })
 }
