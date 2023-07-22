e912f7a22e039c437f48e9963900f2e27268afef
Chadrac-WAKOMYA <chadracxx@gmail.com>
2023-07-22 19:36:32 +0200

    500 error amorced in updatePokemon module

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -7,10 +7,20 @@ module.exports = (app) => {
       where: { id: id }
     })
     .then(_ => {
-      Pokemon.findByPk(id).then(pokemon => {
+      return Pokemon.findByPk(id).then(pokemon => {
+        if(pokemon===null){
+          const message = `Le pokemon demandé n'existe pas. Essayez avec un autre identifiant`
+          const statusCode = '404'
+          return res.status(404).json({statusCode, message})
+        }
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`
         res.json({message, data: pokemon })
       })
+    })
+    .catch(error => {
+      const message = `Le pokemon n'a pas pu être modifié. Réessayez un peu plus tard`
+      const statusCode = '500'
+      res.status(500).json({statusCode, message, data: error})
     })
   })
 }
