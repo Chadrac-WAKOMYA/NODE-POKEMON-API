0f0ff9a151c282bb702a73844da3fdf635e51471
Chadrac-WAKOMYA <chadracxx@gmail.com>
2023-07-22 19:04:05 +0200

    500 error amorced

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require('./src/routes/deletePokemon')(app)
 app.use(({res}) => {
     const message = 'Impossible de trouver la ressource demandée. Vous pouvez essayer une autre url'
     const statusCode = '404'
-    res.status(404).json({message, statusCode})
+    res.status(404).json({statusCode, message})
 })
 
 app.listen(port, () => console.log(`Notre application Node demarre sur : http://localhost:${port}`))
--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -7,5 +7,10 @@ module.exports = (app) => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })
       })
+      .catch(error => {
+        const message = `La liste de pokemons n'a pas pu être récupérée. Réessayez un peu plus tard`
+        const statusCode = '500'
+        res.status(500).json({statusCode, message, data: error})
+      })
   })
 }
